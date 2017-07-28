d4d116307087b5cb852c78041448295fb4b71f89
Felix Meziere <felixm@theodo.co.uk>
2017-07-28 11:18:02 +0100

    Working standard done

---
+++ ios/Pods/Target Support Files/Pods-mb-tvOSTests/Pods-mb-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mb_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_mb_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-mbTests/Pods-mbTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mbTests : NSObject
+@end
+@implementation PodsDummy_Pods_mbTests
+@end
